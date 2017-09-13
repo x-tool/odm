f416ff3d4a54ec9bb522707983b3a963eed34dd6
zgr126 <zgr126@126.com>
2017-09-13 23:23:48 +0800

    RN

--- col.go
+++ col.go
@@ -16,14 +16,17 @@ type Col struct {
 
 func NewCol(d *Database, i interface{}) *Col {
 	c := new(Col)
-	var name string
+	c.Name = GetColName(i)
+	c.DB = d
+	c.Doc = NewDoc(c, i)
+	return c
+}
+
+func GetColName(i interface{}) (name string) {
 	if colI, ok := i.(ColInterface); ok {
 		name = colI.ColName()
 	} else {
 		name = reflect.TypeOf(i).Name()
 	}
-	c.Name = name
-	c.DB = d
-	c.Doc = NewDoc(c, i)
-	return c
+	return
 }
