6ef5aacbcb41ed1c4885eb7f24a38573a8e02255
zgr126 <zgr126@126.com>
2018-12-13 23:18:30 +0800

    rn

--- core/tag.go
+++ core/tag.go
@@ -7,10 +7,9 @@ import (
 // this vars could be user modify, so use var not const
 var (
 	tagName          = "xodm"
-	tagItemSeparator = ":"      // Ex: xodm:"name:value"
-	tagSeparator     = ";"      // Ex: xodm:"name1:value1;name2:value2"
-	tagMark          = "@"      // Ex: xodm:"@mark"
-	tagPath          = "__path" // Ex: xodm:"path" default name to odmTag lst map key
+	tagItemSeparator = ":" // Ex: xodm:"name:value"
+	tagSeparator     = ";" // Ex: xodm:"name1:value1;name2:value2"
+	tagMark          = "@" // Ex: xodm:"@mark"
 )
 
 // `xodm:"@sign"`
@@ -18,7 +17,6 @@ type odmTag struct {
 	sourceTag string
 	mark      string // find docfield quick by custom string
 	allowNull bool
-	path      string // odmPath
 	lst       map[string]string
 }
 
@@ -27,6 +25,7 @@ func newTag(s string) *odmTag {
 	_o.lst = make(map[string]string)
 	_s := strings.TrimSpace(s)
 	_o.sourceTag = _s
+	_o.allowNull = true
 	lst := strings.Split(_s, tagSeparator)
 	for _, v := range lst {
 		fieldLst := strings.Split(v, tagItemSeparator)
@@ -35,13 +34,17 @@ func newTag(s string) *odmTag {
 		var value string
 		// tagMark value like @xxx, so format it diffrent with other
 		if fieldLstLen == 1 {
-			if strings.Index(fieldLst[0], tagMark) == 0 {
+			var _str = fieldLst[0]
+			switch {
+			// notnull
+			case _str == "notnull":
+				_o.allowNull = false
+				name = "notnull"
+				value = ""
+				// @xxx
+			case strings.Index(_str, tagMark) == 0:
 				_o.mark = string([]rune(fieldLst[0])[1:])
 				name = tagMark
-				value = _o.mark
-			} else {
-				_o.path = fieldLst[0]
-				name = tagPath
 				value = _o.mark
 			}
 		} else if fieldLstLen == 2 {
