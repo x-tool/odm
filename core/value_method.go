59b44b01b36591779c4d118a0b3a7e93dab75b8f
zgr126 <zgr126@126.com>
2018-12-18 23:01:50 +0800

    rn

--- core/value_method.go
+++ core/value_method.go
@@ -2,6 +2,8 @@ package core
 
 import (
 	"reflect"
+	"strconv"
+	"strings"
 
 	"github.com/x-tool/tool"
 )
@@ -35,4 +37,42 @@ import (
 
 func ValueToString(value *reflect.Value) (s string) {
 	return tool.ReflectValueToString(value)
+}
+
+func StringToValue(f *StructField, str string) (r reflect.Value, err error) {
+	str = strings.TrimSpace(str)
+	switch f.Kind() {
+	case Bool:
+		var b bool
+		if str == "true" || str == "1" {
+			b = true
+		}
+		r = reflect.ValueOf(true)
+	case Int:
+		_r, err := strconv.ParseInt(str, 10, 64)
+		if err == nil {
+			r = reflect.ValueOf(_r)
+		}
+	case Byte:
+		_r = []byte(str)
+		r = reflect.ValueOf(_r)
+	case Float:
+		_r, err := strconv.ParseFloat(v, 64)
+		if err == nil {
+			r = reflect.ValueOf(_r)
+		}
+	case Complex:
+
+	}
+	return
+	Array
+	Map
+	String
+	Time
+	Date
+	DateTime
+	TimeStamp
+	money
+	Struct
+	Interface
 }
