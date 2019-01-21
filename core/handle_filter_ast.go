b8e2c9d81b58890a9fc86a99176992078f6e8bbd
zgr126 <zgr126@126.com>
2019-01-21 22:10:46 +0800

    rn

--- core/handle_filter_ast.go
+++ core/handle_filter_ast.go
@@ -55,6 +55,7 @@ type ASTTree struct {
 func setBracketsTree(s string) (rootTree ASTTree, err error) {
 	var state_string bool
 	var state_string_esc bool
+	var state_number bool
 	var state_func bool
 	var state_field bool
 	for i, v := range s {
@@ -66,14 +67,65 @@ func setBracketsTree(s string) (rootTree ASTTree, err error) {
 			}
 			if letter == "\"" {
 				state_string = false
-				continue
 			}
+			continue
+		}
+		if state_field  {
+			switch letter{
+			case " ":
+				state_field = false
+			case "(" :
+				state_field = false
+				state_func = true
+			}
+			continue
+		}
+		if state_func {
+			switch letter{
+			case ")" :
+				state_func = false
+			}
+			continue
+		}
+		if state_number {
+			switch letter{
+				case " "
+				state_number = false
+			}
+			continue
 		}
 		findStr := defaultVarsRegexp.FindStringSubmatch(s[i:])
 		if findStr != nil {
 
 		} else {
-
+			switch letter {
+			case "\"":
+				state_string = true
+			case "0":
+				fallthrough
+			case "1":
+				fallthrough
+			case "2":
+				fallthrough
+			case "3":
+				fallthrough
+			case "4":
+				fallthrough
+			case "5":
+				fallthrough
+			case "6":
+				fallthrough
+			case "7":
+				fallthrough
+			case "8":
+				fallthrough
+			case "9":
+				fallthrough
+			case ".":
+				state_number = true
+			default:
+				state_field = true
+			}
 			continue
 		}
 	}
